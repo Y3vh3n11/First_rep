1d3569dcf59d3a5ba32a2b4c536ef00f0f11ac60
e <zheniavlasenko46@gmail.com>
2023-12-15 18:31:37 +0200

    валідація +

--- main.py
+++ main.py
@@ -9,14 +9,35 @@ class Field:
 
 class Name(Field):
     # реалізація класу
+    pass
+    
 
 class Phone(Field):
-    # реалізація класу
+    
+    def __init__(self, phone):        
+        if phone.isdigit() and len(phone) == 10:
+            self.value = phone
+        else:
+            raise ValueError
+   
 
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
+
+    def add_phone(self):
+        pass
+
+    def remove_phone(self):
+        pass
+
+    def edit_phone(self):
+        pass
+
+    def find_phone(self):
+        pass
+
 
     # реалізація класу
 
@@ -25,3 +46,16 @@ class Record:
 
 class AddressBook(UserDict):
     # реалізація класу
+    def add_record(self, name):
+        self.data['name'] = name
+        # додає запис до self.data
+        pass
+
+    def find(self):
+        # шукає за ім'ям 
+        pass
+
+    def delete(self):
+        # видаляє за ім'ям
+        pass
+
