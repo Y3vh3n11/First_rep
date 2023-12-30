291cc431c872508a19c3b78b90e2d0f29b841499
e <zheniavlasenko46@gmail.com>
2023-12-30 11:56:17 +0200

    v2

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ class Field:
  
     def __str__(self):
         return str(self.__value)
+    def __repr__(self):
+        return str(self.__value)
  
 class Name(Field):
     pass
@@ -34,6 +36,8 @@ class Birthday(Field):
             self.__value = birthday
         except ValueError:
             raise ValueError('Формат дати повинен бути ДД.ММ.РРРР')
+    def __str__(self):
+        return str(self.__value)
  
  
 class Phone(Field):
@@ -47,13 +51,15 @@ class Phone(Field):
             self.__value = new_phone
         else:
             raise ValueError('Невірний формат номеру телефону')
+    def __str__(self):
+        return str(self.__value)
  
  
 class Record:
  
     def __init__(self, name, birthday=None):
         self.name = Name(name)
-        self.birthday = Birthday(birthday) if birthday else birthday # якщо др було пепедано, то в клас Birthday передається рядок для перевірки, якщо не було передачі , то self.birthday = None
+        self.birthday = Birthday(birthday) if birthday != None else birthday # якщо др було пепедано, то в клас Birthday передається рядок для перевірки, якщо не було передачі , то self.birthday = None
         self.phones = []
  
     def add_phone(self, phone_str):  # додавання телефону
@@ -75,7 +81,10 @@ class Record:
         for phone in self.phones:
             if phone.value == phone_obj:
                 self.phones.remove(phone)  # видалення телефону
- 
+    
+    def add_birthday(self, birthday_str):
+        self.birthday = Birthday(birthday_str) if birthday_str != None else None
+
     def day_to_birthday(self):
         today = date.today()
         if not self.birthday:
@@ -146,15 +155,161 @@ class AddressBook(UserDict):
     def write_to_file(self): # збереження даних у файл
         val = {}
         for key, values in self.data.items():
-            val[str(key)] = str(values)        
+            val[key] = {
+                'name' : str(values.name),
+                'phones': [str(phone) for phone in values.phones],
+                'birthday': str(values.birthday if values.birthday else '')
+            }               
+        
         with open('AddressBook.json', 'w') as file:
             json.dump(val, file)
 
-    def read_from_file(self): # завантаження даних із файлу
+    def load_from_file(self): # завантаження даних із файлу
         with open('AddressBook.json', 'r') as file:
-            self.data = json.load(file)
+            load_book = json.load(file)                       
+            for key, values in load_book.items():                
+                record = Record(name=str(values['name'].lower()), birthday=values['birthday'] if values['birthday'] else None)
+                if values['phones']:
+                    for phone in values['phones']:
+                        record.add_phone(phone)
+                phone_book.add_record(record)
 
     def __str__(self):
         return '\n'.join(f"{value}" for value in self.data.values())
 
+
+
+def input_error(func):                                                  # докоратор обробки помилок
+    def wrapper(*args, **kwargs):        
+        while True:        
+            try:
+                res = func(*args, **kwargs)          
+            except KeyError:
+                return 'Команда не знайдена'                 
+            except TypeError:
+                return 'Невірні параметри команди'                
+            except IndexError:
+                return 'Введено невірні дані'                
+            else:
+                return res           
+    return wrapper
+
+@input_error
+def add_func(name, num, birthday=None):                                                # функція додавання контактів
+    if name not in str(phone_book.keys()):
+        record = Record(name, birthday)
+        record.add_phone(num)
+        phone_book.add_record(record)
+        return f'Створено контакт {name} із номером {num}'
+    else:
+        return 'Такий контакт вже створено'  
+    
+@input_error
+def change_func(name, num1, num2):                                             # функція зміни контактів
+        if name in str(phone_book.keys()):
+            phone_book[name].edit_phone(num1, num2)
+            return f'Номер {name} змінено з {num1} на {num2}'
+        else:
+            return 'Такого контакту не існує'
+        
+@input_error
+def show_all_func():                                                    # функція відображення всієї книги
+    # phone_book.__str__()
+    return phone_book
+    
+@input_error
+def phone_func(name: str):                                                   # функція відображення одного контакту
+    if name not in str(phone_book.keys()):
+        return 'Такого контакту не існує'
+    else:
+        return f'{phone_book[name]}'
+    
+    
+@input_error
+def bye_func(): 
+    phone_book.write_to_file()
+    return 'Бувай!'
+
+@input_error
+def hello_func():
+    return 'Привіт, чим можу допомогти?'
+
+@input_error
+def day_to_birthday_func(name):                                         # днів до дня народження
+    return phone_book[name].day_to_birthday()
+
+@input_error
+def add_phone_func(name, phone):
+    phone_book[name].add_phone(phone)
+    return f'Користувачу {name} додано телефон {phone}'
+
+@input_error
+def add_birthday_func(name, birthday):
+    phone_book[name].add_birthday(birthday)
+    return f'Користувачеві {name} додано день народження {birthday}'
+
+
+@input_error
+def find_func(find_str):
+    return phone_book.find_new(find_str)
+
+
+@input_error        
+def pars_input(strng):                                                  # розбиття введених даних на команду та аргументи
+    if strng == 'show all':
+        handler_name = 'show all'
+        args = tuple()        
+    elif strng == 'good bye':
+        handler_name = 'good bye'
+        args = tuple()
+    else:
+        split_input = strng.split()
+        handler_name = split_input[0]
+        args = split_input[1:]    
+    return  handler_name, args  
+
+# словник команд 
+cmd_with_args = {'add': add_func,               # додати користувача
+              'change': change_func,            # змінити номер телефону
+              'show all': show_all_func,        # відобразити всю книгу
+              'phone': phone_func,              # відобразити конкретного користувача
+              'exit': bye_func, 'close': bye_func, 'good bye': bye_func,    # завершити роботу та зберегти дані
+              'hello': hello_func,              # привітатися
+              'day_to_birthday': day_to_birthday_func,  # днів до дня народження
+              'add_phone': add_phone_func,              # додати користувачу телефон
+              'add_birthday': add_birthday_func,        # додати користувачу день народження 
+              'find': find_func                         # пошук інформації по рядку
+              }  
+
+@input_error
+def start_cmd(handler_name, args):                                      # виклик команди із списку
+    if args:
+        res = cmd_with_args[handler_name](*args)
+    else:
+        res = cmd_with_args[handler_name]()
+    return res
+
+@input_error
+def main():    
+    try:
+        phone_book.load_from_file()
+    except FileNotFoundError:
+        pass
+    while True:        
+            
+        user_input = input('Чекаю команду: ').lower()
+        result_of_pars = pars_input(user_input)                  # результат обробки введених даних
+        
+        if type(result_of_pars) == str and result_of_pars not in ['show all', 'good bye']:                                 # друк помилки якщо вона виникла
+            print(result_of_pars)
+            continue
+        res = start_cmd(*result_of_pars)    
+        print(res)                                                      # друк результату виконання команди із списку
+        if res == 'Бувай!':
+            break          
+        
+
+if __name__ == '__main__':    
+    phone_book = AddressBook()    
+    main()
    
