cd66fc98c0b449f3fd1713d276a6d756b19b6bcb
Y3vh3n11 <145870816+Y3vh3n11@users.noreply.github.com>
2024-05-06 10:50:55 +0300

    2

--- hw/src/repository/users.py
+++ hw/src/repository/users.py
@@ -4,11 +4,12 @@ from sqlalchemy.orm import Session
 from src.database.models import User
 from src.schemas import UserModel
 
-
+# приймає email та сеанс бази даних db та повертає 
+# об'єкт користувача з бази даних, якщо він існує з такою адресою електронної пошти
 async def get_user_by_email(email: str, db: Session) -> User:
     return db.query(User).filter(User.email == email).first()
 
-
+#  Створює нового користувача у базі даних, а потім повертає щойно створений об'єкт User.
 async def create_user(body: UserModel, db: Session) -> User:
     avatar = None
     try:
@@ -22,7 +23,7 @@ async def create_user(body: UserModel, db: Session) -> User:
     db.refresh(new_user)
     return new_user
 
-
+# оновлює поле refresh_token користувача та фіксує зміни у базі даних.
 async def update_token(user: User, token: str | None, db: Session) -> None:
     user.refresh_token = token
     db.commit()
--- hw/src/schemas.py
+++ hw/src/schemas.py
@@ -1,29 +1,26 @@
 from datetime import date, datetime
-from typing import List, Optional
 from pydantic import BaseModel, Field
 
-
 class ContactModel(BaseModel):
-    first_name: str = Field(max_length=20)
-    last_name: str = Field(max_length=20)
-    email: str = Field(max_length=100)
-    phone_number: str = Field(max_length=20)
+    first_name: str =Field(max_length=20)
+    last_name: str =Field(max_length=20)
+    email: str =Field(max_length=100)
+    phone_number: str =Field(max_length=20)
     birthday: date
-
 
 class ContactResponse(ContactModel):
     id: int
-
     class Config:
         orm_mode = True
 
 
+# представляє корисні дані запиту для створення нового користувача.
 class UserModel(BaseModel):
     username: str = Field(min_length=5, max_length=16)
     email: str
     password: str = Field(min_length=6, max_length=10)
 
-
+# визначає представлення бази даних користувача
 class UserDb(BaseModel):
     id: int
     username: str
@@ -34,12 +31,12 @@ class UserDb(BaseModel):
     class Config:
         orm_mode = True
 
-
+# модель відповіді
 class UserResponse(BaseModel):
     user: UserDb
     detail: str = "User successfully created"
 
-
+# изначає відповідь при отриманні токенів доступу для користувача, що пройшов аутентифікацію.
 class TokenModel(BaseModel):
     access_token: str
     refresh_token: str
